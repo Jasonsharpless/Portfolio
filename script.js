81d1e10f651489eef52acfc55c1e7a6645971133
Jason Sharpless <strawhatsociety@yahoo.com>
2021-02-21 00:09:23 -0600

    semi finished portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+                $(document).ready(function(){
+                  $('.toggle').click(function(){
+                    $('.toggle').toggleClass('active'
+                    $('nav').toggleClass('active')
+                  })
+                })
+
+                
